279acb43a4eb0825a1d606078d5513476290060f
Tea <teamilas@hotmail.com>
2021-02-22 15:08:07 +0000

    responsive -  landscape mobile

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let start = false;
 let score = 0;
 let moves = 0;
 let scoreBoard = document.getElementById("score")
@@ -31,26 +30,47 @@ const deckOfCards = [
     {name: "leaf8", img: "./assets/img/leaf8.png"},
     {name: "leaf9", img: "./assets/img/leaf9.png"}
 ]
+//---------------------------------SHOW/HIDE SECTIONS-----------------------------------------------------
+const showLanding = () => {
+    document.getElementById("landing").classList.remove("hidden");
+    document.getElementById("game_section").classList.add("hidden");
+}
+
+const showGameSection = () => {
+    document.getElementById("game_section").classList.remove("hidden");
+    document.getElementById("landing").classList.add("hidden");
+}
+
+const showLoseMsg = () => {
+    document.getElementById("message-lose").classList.remove("hidden");
+    document.getElementById("message-lose").classList.add("visible");
+}
+
+const showWinMsg = () => {
+    document.getElementById("message-win").classList.remove("hidden");
+    document.getElementById("message-win").classList.add("visible");
+}
+
+const hideLoseMsg = () => {
+    document.getElementById("message-lose").classList.add("hidden");
+    document.getElementById("message-lose").classList.remove("visible");
+}
+
+const hideWinMsg = () => {
+    document.getElementById("message-win").classList.add("hidden");
+    document.getElementById("message-win").classList.remove("visible");
+}
 
 // ------------------------------START GAME--------------------------------------------------------------
 const startGame = () => {
-    start = true;
     
     createGame();
-    document.getElementById("game_section").classList.remove("hidden");
-    document.getElementById("landing").classList.add("hidden");
+    showGameSection();
 
-    
+    let twoMinutes = 60 * 2 ,                                    
+    display = document.getElementById("countdown_time");
 
-    if (start) {
-        console.log("heyaaaa I'm starting the game")
-
-        let twoMinutes = 60 * 2 ,                                    
-        display = document.getElementById("countdown_time");
-
-        console.log()
-        startCountdown(twoMinutes, display);
-    }
+    startCountdown(twoMinutes, display);
 }
 
 //-----------------------------------COUNTDOWN------------------------------------------------------------
@@ -58,29 +78,23 @@ const startCountdown = (duration, display) => {
     let timer = duration, minutes, seconds;
 
     intervalID = setInterval(() => {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
+        minutes = parseInt(timer / 60);
+        seconds = parseInt(timer % 60);
+       
         if (seconds < 10){
             seconds = "0" + seconds
-        } else {
-            seconds = seconds;
-        }
+        } 
 
-        display.textContent = minutes + ":" + seconds;
-
-        console.log("timer is ", timer, duration)
+        display.innerHTML = minutes + ":" + seconds;
 
         if (--timer < 0) {
-            document.getElementById("message-lose").classList.remove("hidden");
-            document.getElementById("message-lose").classList.add("visible");
+            showLoseMsg();
             clearInterval(intervalID);
         } 
 
     },1000);
 }
-//-------------------------------STOP COUNTDOWN------------------------------------------------------------
-
+//-------------------------------------STOP COUNTDOWN-----------------------------------------------------
 const stopCountdown = () => {
     console.log("TIME STOP PLS")
     clearInterval(intervalID);
@@ -96,9 +110,8 @@ const shuffleArray = (array) => {
     }
 }
 
-//-------------------------------------CREATE  CARDS------------------------------------------------------------
+//-------------------------------------CREATE  CARDS---------------------------------------------------------
 const createGame = () => {
-    console.log("creating the boardd")
     shuffleArray(deckOfCards);
 
     deckOfCards.forEach((card,index) => { 
@@ -109,7 +122,7 @@ const createGame = () => {
         cardsGrid.appendChild(div);   
     })
 
-    //---------------------------------ADD CLICK EVENT TO THE CARD---------------------------
+    //---------------------------------ADD CLICK EVENT TO THE CARD--------------------------------------------
     //get node list
     divs = document.querySelectorAll("div");
     //turn it into an array
@@ -118,27 +131,21 @@ const createGame = () => {
     cardsArray.forEach(card => card.addEventListener("click", clickedCard));
 }
 
-
-
-
-//-------------------------------CLICKED CARD--------------------------------------------------------------
+//-------------------------------CLICKED CARD-----------------------------------------------------------------
     
 const clickedCard = (event) => { 
-    console.log("ok ok", event.target)
-    // gets the name so we know which leaf was clicked
+    // gets the name so we know which leaf was clicked  (leaf1,leaf2....)
     const leaf = event.target.getAttribute("data-leaf");
-    console.log("leaf is",leaf)
 
     if (event.target.classList.contains("matched")) {
-        console.log("MISTAKEEE", leaf)
         return;
     }
     
     twoCards.push(leaf)
-
     matchCards(leaf,event);
 }
-//-----------------------------------MATCH CARDS---------------------------------------
+
+//-----------------------------------MATCH CARDS--------------------------------------------------------------
 
 const matchCards = (leaf,event) => {
     if (twoCards.length == 1) {
@@ -187,14 +194,11 @@ const matchCards = (leaf,event) => {
 
 
 //-------------------------------------------ADD SCORE + MOVES------------------------------------------
-
 const addScore = () => {
     if (score ==  8) {
-        console.log(score)
         document.getElementById("totalMoves").innerHTML = moves + 1;
         scoreBoard.innerHTML = score + 1;
-        document.getElementById("message-win").classList.remove("hidden");
-        document.getElementById("message-win").classList.add("visible");
+        showWinMsg();
         stopCountdown();
 
     } else {
@@ -207,21 +211,26 @@ const addMoves = () => {
     moves = moves + 1;
     movesBoard.innerHTML = moves;
 }
-
-
-//---------------------------------------PLAY AGAIN----------------------------------------------------------
-const replay = () => {
-    document.getElementById("message-win").classList.add("hidden");
-    document.getElementById("message-win").classList.remove("visible");
-
-    document.getElementById("message-lose").classList.add("hidden");
-    document.getElementById("message-lose").classList.remove("visible");
-
+//----------------------------------------CLEAR BOARD-----------------------------------------------------
+const clearBoard = () => {
     cardsGrid.innerHTML = "";
     score = 0;
     moves = 0; 
     movesBoard.innerHTML = 0; 
     scoreBoard.innerHTML = 0; 
+}
+
+//---------------------------------------PLAY AGAIN--------------------------------------------------------
+const replay = () => {
+    hideWinMsg();
+    hideLoseMsg();
+    clearBoard();
     startGame();
 }
 
+const toStart = () => {
+    hideWinMsg();
+    hideLoseMsg();
+    clearBoard();
+    showLanding();
+}
